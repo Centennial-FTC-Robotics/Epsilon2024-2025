3cc25f2c25d428a7876e9246eba4a51363b6572c
Christian <cha.christian.s@gmail.com>
2024-11-17 16:11:37 -0500

    Created Drivetrain in Subsystens package for auto methods.SmpleTeleop is finished

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAuto.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
+
+@Autonomous(name = "SampleAuto")
+public class SampleAuto extends LinearOpMode {
+
+
+    private DcMotorEx slideMotorR;
+    private DcMotorEx slideMotorL;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        slideMotorR = hardwareMap.get(DcMotorEx.class, "slideMotorRight");
+        slideMotorL = hardwareMap.get(DcMotorEx.class, "slideMotorLeft");
+
+        slideMotorR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slideMotorL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+        Drivetrain drivetrain = new Drivetrain(hardwareMap);
+
+        waitForStart();
+        drivetrain.move(24.0,"strafe");
+
+
+
+        drivetrain.move(12.0, "drive");
+
+
+    }
+}
